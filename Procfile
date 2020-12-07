9fc45cb76c360cf9c051d41a0940430ee357ea13
Drew Ayling <dsayling@gmail.com>
2020-12-07 14:21:10 -0700

    Create action to run config variant
    Rather than use circleci and confuse users with the circleci config fie, we can use an action to ensure all the config variants are tested
    
    Move actions file
    
    Add heroku deployment as first variant
    
    Adding diff file
    
    try matrix
    
    copy isn't persisting
    
    try specifying pr sha
    
    fix workflow
    
    cant get it to commit
    
    blah?
    
    forceadd?
    
    can you do this?
    
    maybe this?
    
    check for parity
    
    make entrypoint exec
    
    switch to new branch with -b not -B
    
    remove debug
    
    add git status
    
    remove force-add
    
    try again
    
    specify name and email for commit
    
    this should work now
    
    trying branch naming scheme
    
    maybe like this?
    
    check magicvar
    
    adding pylint variant
    
    remove rebase
    
    forget about child branches for a minute

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn openapi_server.__main__:app
--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -5,12 +5,12 @@ import connexion
 from openapi_server import encoder
 from flask_cors import CORS
 
+app = connexion.App(__name__, specification_dir="./openapi/")
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api("openapi.yaml", arguments={"title": "CFD"}, pythonic_params=True)
+CORS(app.app)
 
 def main():
-    app = connexion.App(__name__, specification_dir="./openapi/")
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api("openapi.yaml", arguments={"title": "CFD"}, pythonic_params=True)
-    CORS(app.app)
     app.run(port=8080)
 
 
