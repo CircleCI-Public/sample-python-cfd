e888fcbf5afcbcd04fa03d3142f92079c1619ce0
Drew Ayling <dsayling@gmail.com>
2021-02-23 12:22:06 -0700

    fix sqlalchemy instantiation

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -12,7 +12,7 @@ app.app.json_encoder = encoder.JSONEncoder
 app.app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.add_api("openapi.yaml", arguments={"title": "CFD"}, pythonic_params=True)
 CORS(app.app)
-db = SQLAlchemy(app)
+db = SQLAlchemy(app.app)
 
 def main():
     app.run(port=8000)
