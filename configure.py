2533557e5ef276cd7d8a9abce959c3a4be30f0b8
Drew Ayling <dsayling@gmail.com>
2021-03-09 13:28:29 -0700

    update configure script to properly seed db

--- configure.py
+++ configure.py
@@ -1,4 +1,8 @@
+import argparse
+
 from openapi_server.__main__ import models, app
+from openapi_server.database import db_seed
+
 
 
 def create_db():
@@ -8,11 +12,10 @@ def create_db():
     models.db.session.commit()
 
 
-def seed_db():
-    # db.session.add(item)
-    # db.session.commit()
-    pass
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', action='store_true', default=False)
+    args = parser.parse_args()
     create_db()
+    if args.seed:
+        db_seed()
